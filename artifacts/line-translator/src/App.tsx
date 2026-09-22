48f65b072729e9f004956109f4fcbbe8d7cbb27f
mastercaste2-hub <mastercaste2@gmail.com>
2026-09-22 20:51:10 +0200

    Update App.tsx

--- artifacts/line-translator/src/App.tsx
+++ artifacts/line-translator/src/App.tsx
@@ -36,7 +36,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, '')}>
+    <WouterRouter>
           <Router />
         </WouterRouter>
         <Toaster />
